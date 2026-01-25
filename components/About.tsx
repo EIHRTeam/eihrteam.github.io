31d933802443963962b48728b3aee90edbafec83
u-00a0 <null@hcaor.org>
2026-01-25 19:07:38 +0800

    style: use repeating background for color-bar to ensure complete display

--- components/About.tsx
+++ components/About.tsx
@@ -12,15 +12,17 @@ const About: React.FC<AboutProps> = ({ content }) => {
   return (
     <Section id="about" className="pt-20">
       <GlitchElement>
-        <div className="flex gap-16">
-           {/* Vertical Color Bar */}
-           <div className="hidden md:block w-8 flex-shrink-0 relative">
-             <img 
-               src={colorBar} 
-               alt="" 
-               className="absolute top-0 left-1/2 -translate-x-1/2 h-full min-w-[16px] w-auto max-w-none object-cover"
-             />
-           </div>
+        <div className="flex gap-12">
+           {/* Vertical Color Bar - Repeating to show full pattern */}
+           <div 
+             className="hidden md:block w-4 flex-shrink-0"
+             style={{ 
+               backgroundImage: `url(${colorBar})`,
+               backgroundRepeat: 'repeat-y',
+               backgroundSize: '100% auto',
+               backgroundPosition: 'center'
+             }}
+           />
            
            <div className="grid md:grid-cols-2 gap-8 pl-4 md:pl-0 border-l-4 border-black md:border-none">
             {/* Mobile shows border-l, Desktop shows image bar */}
